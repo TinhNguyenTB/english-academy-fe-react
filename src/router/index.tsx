b66fa038191557005461eac3c7257f8fe0169513
TinhNguyenTB <tinh271103@gmail.com>
2025-07-14 16:40:06 +0700

    add paths const

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,15 @@
-import Home from "@/pages/Home"
-import Login from "@/pages/Login"
-import { createBrowserRouter } from "react-router-dom"
-
+import { PATHS } from '@/constants/paths'
+import Home from '@/pages/Home'
+import Login from '@/pages/Login'
+import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    path: PATHS.HOME,
+    element: <Home />
   },
   {
-    path: "/login",
-    element: <Login />,
-  },
+    path: PATHS.LOGIN,
+    element: <Login />
+  }
 ])
