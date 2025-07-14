cb887cad5057f351f6bc00c36babc88ca88fdbde
TinhNguyenTB <tinh271103@gmail.com>
2025-07-14 14:25:33 +0700

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/typescript',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,9 @@
+
+
+const MyComponent = () => {
+    return (
+        <div className="text-amber-500">MyComponent</div>
+    )
+}
+
+export default MyComponent
