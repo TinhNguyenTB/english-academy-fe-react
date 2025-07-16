a7daa6253789d4c3ba05c5cf1d9aa92588d58096
TinhNguyenTB <tinh271103@gmail.com>
2025-07-16 20:19:27 +0700

    refactor components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { LanguageSwitcher } from '@/components/Customs/LanguageSwitcher'
+import { LanguageSwitcher } from '@/components/Atoms/LanguageSwitcher'
 import { useTranslation } from 'react-i18next'
 
 export default function Home() {
